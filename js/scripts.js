5ec92ff0a400c16f616786ef32ed111dca57ed21
shmishtopher <shmish90@gmail.com>
2016-09-17 16:39:57 -0400

    almost working grid

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-$( function() {
-  $( "#preview" ).resizable({
-    handles: "s"
-  });
-  } );
--- js/ui.js
+++
@@ -1,6 +0,0 @@
-function open() {
-    document.getElementById("mySidenav").style.display = "block";
-}
-function close() {
-    document.getElementById("mySidenav").style.display = "none";
-}
