87c362d1d286fb5fc63ab51ec54c32fe126becdf
shmishtopher <shmish90@gmail.com>
2016-09-18 16:27:07 -0400

    ITS ALIVE!

--- js/addElement.js
+++ js/addElement.js
@@ -1,5 +1,8 @@
+$('.grid-stack').gridstack();
+var grid = $('.grid-stack').data('gridstack');
+
 function addElement(type) {
   if(type === 'blank') {
-    $('.grid-stack').append('<div class="grid-stack-item"data-gs-x="0" data-gs-y="0"data-gs-width="4" data-gs-height="2"><div id="item" class="grid-stack-item-content">Drag me</div></div>');
+    grid.addWidget('<div class="grid-stack-item"data-gs-x="4" data-gs-y="0"data-gs-width="4" data-gs-height="2"><div id="item" class="grid-stack-item-content">Drag me</div></div>', 0, 0, 3, 2, true);
   }
 }
